ce5fd26df79af6597391c39e0475697d392fb182
Junior Programmer <progphyton@gmail.com>
2023-04-24 17:05:09 +0300

    fix(extended-1.2): add alt description to image in JS

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.classList.add("button", "delete");
     deleteButtonImg.src="./remove.svg";
     deleteButtonImg.className="delete-image";
+    deleteButtonImg.setAttribute("alt","Delete");
     deleteButton.appendChild(deleteButtonImg);
 
 
